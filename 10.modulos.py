7bd75d946317a0a1a3109a21ca45bf86f3dd961b
antsu comuna <acampospsantos@hotmail.com>
2024-10-22 17:09:06 -0300

    Update modulos

--- 10.modulos.py
+++ 10.modulos.py
@@ -1,1 +1,15 @@
- 
+#Módulos são pacotes que podem ser adicionados ao nosso programa
+# com intuito de auxiliar 
+# Exemplo: importar módulo math para termos acesso a funções matemáticas mais específicas
+
+#Vamos para um exemplo
+import math # --> Tenho acesso a todas funções da biblioteca math
+
+from math import sqrt # --> Tenho acesso apenas a função sqrt da biblioteca math
+
+math.ceil #Arredonda o número pra cima
+math.floor # Arredonda o número pra baixo
+math.trunc #Elimina os números pós vírgula
+math.pow #Cálculo da Potência
+math.sqrt #Calcula a raiz quadrada
+math.factorial #Calcula o fatorial
